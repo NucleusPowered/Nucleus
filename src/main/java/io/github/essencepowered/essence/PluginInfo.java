5c14ca2e9fb90360cd756aa21fea1e165ec72561
kinggoesgaming <hunar.roop@gmail.com>
2016-03-01 12:36:56 -0800

    Add missing headers

--- src/main/java/io/github/essencepowered/essence/PluginInfo.java
+++ src/main/java/io/github/essencepowered/essence/PluginInfo.java
@@ -1,3 +1,7 @@
+/*
+ * This file is part of Essence, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
 package io.github.essencepowered.essence;
 
 import org.spongepowered.api.text.Text;
