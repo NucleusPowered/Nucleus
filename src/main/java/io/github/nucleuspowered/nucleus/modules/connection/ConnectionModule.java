66d5bea5af4dd51f4ae01a2d5fa721c354ce1a73
Oblivion <hasaan668@hotmail.co.uk>
2016-07-28 15:48:33 +0100

    Allow users with the correct permission to join the server if full

---
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/connection/ConnectionModule.java
@@ -0,0 +1,10 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
+package io.github.nucleuspowered.nucleus.modules.connection;
+
+import uk.co.drnaylor.quickstart.annotations.ModuleData;
+
+@ModuleData(id = "connection", name = "Connection")
+public class ConnectionModule { }
