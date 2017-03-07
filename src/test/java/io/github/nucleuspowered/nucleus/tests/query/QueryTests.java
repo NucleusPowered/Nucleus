758799c34cdb714a275b3a356eb0db612d73faea
Hasaan Arshad <hasaan668@hotmail.co.uk>
2017-03-07 17:41:33 +0000

    Add missing licenses

--- src/test/java/io/github/nucleuspowered/nucleus/tests/query/QueryTests.java
+++ src/test/java/io/github/nucleuspowered/nucleus/tests/query/QueryTests.java
@@ -1,3 +1,7 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
 package io.github.nucleuspowered.nucleus.tests.query;
 
 import io.github.nucleuspowered.nucleus.api.query.QueryComparator;
