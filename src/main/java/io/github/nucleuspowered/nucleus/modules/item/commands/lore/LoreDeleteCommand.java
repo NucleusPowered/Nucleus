a0caaa2fa26bae36206a1e5a22ad6e0d8e820a04
Nick <nick.degruccio@gmail.com>
2017-03-17 00:22:10 -0700

    Add more /lore commands
    Adds:
    - /lore delete <line>
    - /lore insert <line> <text>
    - /lore edit <line> <text>

---
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/item/commands/lore/LoreDeleteCommand.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Nucleus, licensed under the MIT License (MIT). See the LICENSE.txt file
+ * at the root of this project for more details.
+ */
+package io.github.nucleuspowered.nucleus.modules.item.commands.lore;
+
+import com.google.inject.Inject;
+import io.github.nucleuspowered.nucleus.argumentparsers.MessageTargetArgument;
+import io.github.nucleuspowered.nucleus.argumentparsers.NicknameArgument;
+import io.github.nucleuspowered.nucleus.argumentparsers.SelectorWrapperArgument;
+import io.github.nucleuspowered.nucleus.internal.annotations.Permissions;
+import io.github.nucleuspowered.nucleus.internal.annotations.RegisterCommand;
+import io.github.nucleuspowered.nucleus.internal.command.AbstractCommand;
+import io.github.nucleuspowered.nucleus.internal.messages.MessageProvider;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.CommandElement;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.item.LoreData;
+import org.spongepowered.api.data.type.HandTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.text.Text;
+
+import java.util.List;
+
+@Permissions(prefix = "lore", mainOverride = "set")
+@RegisterCommand(value = "delete", subcommandOf = LoreCommand.class)
+public class LoreDeleteCommand extends AbstractCommand<Player> {
+
+    @Inject private MessageProvider provider;
+
+    private final String loreLine = "line";
+
+    @Override
+    public CommandElement[] getArguments() {
+        return new CommandElement[] {
+                GenericArguments.integer(Text.of(loreLine))
+        };
+    }
+
+    @Override
+    protected CommandResult executeCommand(Player src, CommandContext args) throws Exception {
+        if (!src.getItemInHand(HandTypes.MAIN_HAND).isPresent()) {
+            src.sendMessage(provider.getTextMessageWithFormat("command.lore.clear.noitem"));
+            return CommandResult.empty();
+        }
+
+        ItemStack stack = src.getItemInHand(HandTypes.MAIN_HAND).get();
+        LoreData loreData = stack.getOrCreate(LoreData.class).get();
+
+        int line = args.<Integer>getOne(loreLine).get();
+        List<Text> loreList = loreData.lore().get();
+        if(loreList.size() <= line){
+            src.sendMessage(provider.getTextMessageWithFormat("command.lore.set.invalidLine"));
+            return CommandResult.empty();
+        }
+
+        loreList.remove(line);
+
+        if (stack.offer(Keys.ITEM_LORE, loreList).isSuccessful()) {
+            src.setItemInHand(HandTypes.MAIN_HAND, stack);
+
+            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.success"));
+            return CommandResult.success();
+        }
+
+        src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.fail"));
+        return CommandResult.empty();
+    }
+}
--- src/main/java/io/github/nucleuspowered/nucleus/modules/item/commands/lore/LoreSetBaseCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/item/commands/lore/LoreSetBaseCommand.java
@@ -26,10 +26,12 @@ abstract class LoreSetBaseCommand extends AbstractCommand<Player> {
     @Inject private MessageProvider provider;
 
     final String loreKey = "lore";
+    final String loreLine = "line";
 
     @Override
     public CommandElement[] getArguments() {
         return new CommandElement[] {
+                GenericArguments.optionalWeak(GenericArguments.integer(Text.of(loreLine))),
                 GenericArguments.remainingJoinedStrings(Text.of(loreKey))
         };
     }
@@ -51,6 +53,60 @@ abstract class LoreSetBaseCommand extends AbstractCommand<Player> {
         } else {
             loreList = loreData.lore().get();
             loreList.add(getLore);
+        }
+
+        if (stack.offer(Keys.ITEM_LORE, loreList).isSuccessful()) {
+            src.setItemInHand(HandTypes.MAIN_HAND, stack);
+
+            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.success"));
+            return CommandResult.success();
+        }
+
+        src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.fail"));
+        return CommandResult.empty();
+    }
+
+    /**
+     * Like the method above, this method is to update existing lore to the item.
+     * When 'editOrInsert' is true, we will edit the lore at the passed line to
+     * the passed text. When false, we will rather insert the lore at the specified
+     * line.
+     *
+     * @param src The player attempting to alter an item's lore
+     * @param message The text to offer to the item
+     * @param line The line of the lore we want to edit
+     * @param editOrInsert True to edit, false to insert
+     * @return The result of the operation
+     */
+    CommandResult setLore(Player src, String message, int line, boolean editOrInsert){
+        if(line < 0){
+            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.invalidLine"));
+            return CommandResult.empty();
+        }
+
+        if(!src.getItemInHand(HandTypes.MAIN_HAND).isPresent()){
+            src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.noitem"));
+            return CommandResult.empty();
+        }
+
+        ItemStack stack = src.getItemInHand(HandTypes.MAIN_HAND).get();
+        LoreData loreData = stack.getOrCreate(LoreData.class).get();
+
+        Text getLore = TextSerializers.FORMATTING_CODE.deserialize(message);
+
+        List<Text> loreList = loreData.lore().get();
+        if(editOrInsert){
+            if(loreList.size() <= line){
+                src.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("command.lore.set.invalidEdit"));
+                return CommandResult.empty();
+            }
+            loreList.set(line, getLore);
+        } else {
+            if(loreList.size() <= line){
+                loreList.add(getLore);
+            } else {
+                loreList.add(line, getLore);
+            }
         }
 
         if (stack.offer(Keys.ITEM_LORE, loreList).isSuccessful()) {
