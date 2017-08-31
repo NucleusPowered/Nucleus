e01c1a20831e320b21309591ee00d15edd74f250
Mohron <mohron@mohron.net>
2017-08-31 18:16:33 -0500

    Update World Perm listener to use SafeLocationWithBlacklist

--- src/main/java/io/github/nucleuspowered/nucleus/modules/world/listeners/WorldListener.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/world/listeners/WorldListener.java
@@ -4,39 +4,57 @@
  */
 package io.github.nucleuspowered.nucleus.modules.world.listeners;
 
-import io.github.nucleuspowered.nucleus.Nucleus;
 import io.github.nucleuspowered.nucleus.internal.ListenerBase;
 import io.github.nucleuspowered.nucleus.internal.PermissionRegistry;
-import io.github.nucleuspowered.nucleus.internal.annotations.ConditionalListener;
 import io.github.nucleuspowered.nucleus.modules.world.WorldModule;
 import io.github.nucleuspowered.nucleus.modules.world.config.WorldConfig;
 import io.github.nucleuspowered.nucleus.modules.world.config.WorldConfigAdapter;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.filter.Getter;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.teleport.TeleportHelperFilters;
 
-import java.util.function.Predicate;
+import java.util.Optional;
+import java.util.function.Consumer;
 
-@ConditionalListener(WorldListener.Condition.class)
-public class WorldListener extends ListenerBase {
+public class WorldListener extends ListenerBase implements ListenerBase.Conditional {
 
-    @Listener
-    public void onPlayerTeleport(MoveEntityEvent.Teleport event, @Getter("getTargetEntity") Player player) {
-        World target = event.getToTransform().getExtent();
-        if (player.getWorld().equals(target)) return;
+	@Listener
+	public void onPlayerTeleport(MoveEntityEvent.Teleport event, @Getter("getTargetEntity") Player player) {
+		World target = event.getToTransform().getExtent();
+		if (player.getWorld().equals(target)) return;
 
-        if (!player.hasPermission(PermissionRegistry.PERMISSIONS_PREFIX + "worlds." + target.getName().toLowerCase())) {
-            event.setCancelled(true);
-            player.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("world.access.denied", target.getName()));
-        }
-    }
+		if (!player.hasPermission(PermissionRegistry.PERMISSIONS_PREFIX + "worlds." + target.getName().toLowerCase())) {
+			event.setCancelled(true);
+			player.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat("world.access.denied", target.getName()));
+			if (event instanceof MoveEntityEvent.Teleport.Portal) {
+				Sponge.getScheduler().createTaskBuilder()
+					.delayTicks(1)
+					.execute(relocate(player))
+					.submit(plugin);
+			}
+		}
+	}
 
-    public static class Condition implements Predicate<Nucleus> {
+	@Override
+	public boolean shouldEnable() {
+		return plugin.getConfigValue(WorldModule.ID, WorldConfigAdapter.class, WorldConfig::isSeparatePermissions).orElse(false);
+	}
 
-        @Override public boolean test(Nucleus nucleus) {
-            return nucleus.getConfigValue(WorldModule.ID, WorldConfigAdapter.class, WorldConfig::isSeparatePermissions).orElse(false);
-        }
-    }
+	private Consumer<Task> relocate(Player player) {
+		return src -> {
+			Optional<Location<World>> location = Sponge.getTeleportHelper().getSafeLocationWithBlacklist(player.getLocation(), 5, 5, 5, TeleportHelperFilters.NO_PORTAL);
+			if (location.isPresent()) {
+				player.setLocation(location.get());
+			} else {
+				player.setLocationSafely(player.getWorld().getSpawnLocation());
+			}
+		};
+	}
+
 }
