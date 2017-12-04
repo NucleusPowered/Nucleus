360b33b49dfff58710ffdefa3b7993461f3b22ec
Mohron <mohron@mohron.net>
2017-12-03 19:40:59 -0600

    Refactor repair result tracking to not use fields.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/item/commands/RepairCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/item/commands/RepairCommand.java
@@ -12,7 +12,6 @@ import io.github.nucleuspowered.nucleus.internal.command.ReturnMessageException;
 import io.github.nucleuspowered.nucleus.internal.docgen.annotations.EssentialsEquivalent;
 import io.github.nucleuspowered.nucleus.internal.interfaces.Reloadable;
 import io.github.nucleuspowered.nucleus.modules.item.config.ItemConfigAdapter;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.args.CommandElement;
@@ -27,30 +26,20 @@ import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.item.inventory.entity.Hotbar;
-import org.spongepowered.api.item.inventory.equipment.EquipmentType;
-import org.spongepowered.api.item.inventory.equipment.WornEquipmentType;
+import org.spongepowered.api.item.inventory.equipment.EquipmentInventory;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
-import java.util.Optional;
 
 @NonnullByDefault
 @Permissions(supportsOthers = true)
 @RegisterCommand({"repair", "mend"})
 @EssentialsEquivalent({"repair", "fix"})
 public class RepairCommand extends AbstractCommand<Player> implements Reloadable {
-
-    private int successCount = 0;
-    private ItemStackSnapshot lastSuccess = ItemStackSnapshot.NONE;
-    private int restrictedCount = 0;
-    private ItemStackSnapshot lastRestricted = ItemStackSnapshot.NONE;
-    private int errorCount = 0;
-    private ItemStackSnapshot lastError = ItemStackSnapshot.NONE;
-    private int noDurabilityCount = 0;
-    private ItemStackSnapshot lastNoDurability = ItemStackSnapshot.NONE;
 
     private boolean whitelist = false;
     private List<ItemType> restrictions = new ArrayList<>();
@@ -75,12 +64,25 @@ public class RepairCommand extends AbstractCommand<Player> implements Reloadable
     }
 
     @Override protected CommandResult executeCommand(Player pl, CommandContext args) throws Exception {
+        EnumMap<ResultType, Integer> resultCount = new EnumMap(ResultType.class) {{
+            put(ResultType.SUCCESS, 0);
+            put(ResultType.ERROR, 0);
+            put(ResultType.NO_DURABILITY, 0);
+            put(ResultType.RESTRICTED, 0);
+        }};
+        EnumMap<ResultType, ItemStackSnapshot> lastItem = new EnumMap<>(ResultType.class);
+
         String location = "inventory";
         if (args.hasAny("a")) {
             for (Inventory slot : pl.getInventory().slots()) {
                 if (slot.peek().isPresent() && !slot.peek().get().isEmpty()) {
                     ItemStack stack = slot.peek().get();
-                    repairStack(stack).ifPresent(slot::set);
+                    RepairResult result = repairStack(stack);
+                    resultCount.compute(result.type, (t, i) -> i += 1);
+                    lastItem.put(result.type, result.stack.createSnapshot());
+                    if (result.isSuccessful()) {
+                        slot.set(result.stack);
+                    }
                 }
             }
         } else {
@@ -103,21 +105,36 @@ public class RepairCommand extends AbstractCommand<Player> implements Reloadable
             // Repair item in main hand
             if (repairMainhand && pl.getItemInHand(HandTypes.MAIN_HAND).isPresent()) {
                 ItemStack stack = pl.getItemInHand(HandTypes.MAIN_HAND).get();
-                repairStack(stack).ifPresent(s -> pl.setItemInHand(HandTypes.MAIN_HAND, s));
+                RepairResult result = repairStack(stack);
+                resultCount.compute(result.type, (t, i) -> i += 1);
+                lastItem.put(result.type, result.stack.createSnapshot());
+                if (result.isSuccessful()) {
+                    pl.setItemInHand(HandTypes.MAIN_HAND, result.stack);
+                }
             }
 
             // Repair item in off hand
             if (repairOffhand && pl.getItemInHand(HandTypes.OFF_HAND).isPresent()) {
                 ItemStack stack = pl.getItemInHand(HandTypes.OFF_HAND).get();
-                repairStack(stack).ifPresent(s -> pl.setItemInHand(HandTypes.OFF_HAND, s));
+                RepairResult result = repairStack(stack);
+                resultCount.compute(result.type, (t, i) -> i += 1);
+                lastItem.put(result.type, result.stack.createSnapshot());
+                if (result.isSuccessful()) {
+                    pl.setItemInHand(HandTypes.OFF_HAND, result.stack);
+                }
             }
 
             // Repair worn equipment
             if (repairEquip) {
-                for (EquipmentType type : Sponge.getRegistry().getAllOf(WornEquipmentType.class)) {
-                    if (pl.getEquipped(type).isPresent()) {
-                        ItemStack stack = pl.getEquipped(type).get();
-                        repairStack(stack).ifPresent(s -> pl.equip(type, s));
+                for (Inventory slot : pl.getInventory().query(EquipmentInventory.class).slots()) {
+                    if (slot.peek().isPresent() && !slot.peek().get().isEmpty()) {
+                        ItemStack stack = slot.peek().get();
+                        RepairResult result = repairStack(stack);
+                        resultCount.compute(result.type, (t, i) -> i += 1);
+                        lastItem.put(result.type, result.stack.createSnapshot());
+                        if (result.isSuccessful()) {
+                            slot.set(result.stack);
+                        }
                     }
                 }
             }
@@ -127,23 +144,32 @@ public class RepairCommand extends AbstractCommand<Player> implements Reloadable
                 for (Inventory slot : pl.getInventory().query(Hotbar.class).slots()) {
                     if (slot.peek().isPresent() && !slot.peek().get().isEmpty()) {
                         ItemStack stack = slot.peek().get();
-                        repairStack(stack).ifPresent(slot::set);
+                        RepairResult result = repairStack(stack);
+                        resultCount.compute(result.type, (t, i) -> i += 1);
+                        lastItem.put(result.type, result.stack.createSnapshot());
+                        if (result.isSuccessful()) {
+                            slot.set(result.stack);
+                        }
                     }
                 }
             }
         }
 
-        location = plugin.getMessageProvider().getMessageFromKey("repair.command.location." + location).get();
-        if (successCount == 0 && errorCount == 0 && noDurabilityCount == 0) {
+        location = plugin.getMessageProvider().getMessageFromKey("command.repair.location." + location).orElse("inventory");
+
+        if (resultCount.get(ResultType.SUCCESS) == 0 && resultCount.get(ResultType.ERROR) == 0
+                && resultCount.get(ResultType.NO_DURABILITY) == 0 && resultCount.get(ResultType.RESTRICTED) == 0) {
             throw ReturnMessageException.fromKey("command.repair.empty", pl.getName(), location);
         } else {
             // Non-repairable Message - Only used when all items processed had no durability
-            if (noDurabilityCount > 0 && successCount == 0 && errorCount == 0) {
-                if (noDurabilityCount == 1) {
+            if (resultCount.get(ResultType.NO_DURABILITY) > 0 && resultCount.get(ResultType.SUCCESS) == 0
+                    && resultCount.get(ResultType.ERROR) == 0 && resultCount.get(ResultType.RESTRICTED) == 0) {
+                if (resultCount.get(ResultType.NO_DURABILITY) == 1) {
+                    ItemStackSnapshot item = lastItem.get(ResultType.NO_DURABILITY);
                     pl.sendMessage(plugin.getMessageProvider().getTextMessageWithTextFormat(
                             "command.repair.nodurability.single",
-                            Text.builder(lastNoDurability.getTranslation().get())
-                                    .onHover(TextActions.showItem(lastNoDurability))
+                            item.get(Keys.DISPLAY_NAME).orElse(Text.of(item.getTranslation().get())).toBuilder()
+                                    .onHover(TextActions.showItem(item))
                                     .build(),
                             Text.of(pl.getName()),
                             Text.of(location)
@@ -151,87 +177,101 @@ public class RepairCommand extends AbstractCommand<Player> implements Reloadable
                 } else {
                     pl.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat(
                             "command.repair.nodurability.multiple",
-                            Integer.toString(noDurabilityCount), pl.getName(), location
+                            resultCount.get(ResultType.NO_DURABILITY).toString(), pl.getName(), location
                     ));
                 }
             }
 
             // Success Message
-            if (successCount == 1) {
+            if (resultCount.get(ResultType.SUCCESS) == 1) {
+                ItemStackSnapshot item = lastItem.get(ResultType.SUCCESS);
                 pl.sendMessage(plugin.getMessageProvider().getTextMessageWithTextFormat(
                         "command.repair.success.single",
-                        Text.builder(lastSuccess.getTranslation().get())
-                                .onHover(TextActions.showItem(lastSuccess))
+                        item.get(Keys.DISPLAY_NAME).orElse(Text.of(item.getTranslation().get())).toBuilder()
+                                .onHover(TextActions.showItem(item))
                                 .build(),
                         Text.of(pl.getName()),
                         Text.of(location)
                 ));
-            } else if (successCount > 1) {
+            } else if (resultCount.get(ResultType.SUCCESS) > 1) {
                 pl.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat(
                         "command.repair.success.multiple",
-                        Integer.toString(successCount), pl.getName(), location
+                        resultCount.get(ResultType.SUCCESS).toString(), pl.getName(), location
                 ));
             }
 
             // Error Message
-            if (errorCount == 1) {
+            if (resultCount.get(ResultType.ERROR) == 1) {
+                ItemStackSnapshot item = lastItem.get(ResultType.ERROR);
                 pl.sendMessage(plugin.getMessageProvider().getTextMessageWithTextFormat(
                         "command.repair.error.single",
-                        Text.builder(lastError.getTranslation().get())
-                                .onHover(TextActions.showItem(lastError))
+                        item.get(Keys.DISPLAY_NAME).orElse(Text.of(item.getTranslation().get())).toBuilder()
+                                .onHover(TextActions.showItem(item))
                                 .build(),
                         Text.of(pl.getName()),
                         Text.of(location)
                 ));
-            } else if (errorCount > 1) {
+            } else if (resultCount.get(ResultType.ERROR) > 1) {
                 pl.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat(
                         "command.repair.error.multiple",
-                        Integer.toString(errorCount), pl.getName(), location
+                        resultCount.get(ResultType.ERROR).toString(), pl.getName(), location
                 ));
             }
 
             // Restriction Message
-            if (restrictedCount == 1) {
+            if (resultCount.get(ResultType.RESTRICTED) == 1) {
+                ItemStackSnapshot item = lastItem.get(ResultType.RESTRICTED);
                 pl.sendMessage(plugin.getMessageProvider().getTextMessageWithTextFormat(
                         "command.repair.restricted.single",
-                        Text.builder(lastRestricted.getTranslation().get())
-                                .onHover(TextActions.showItem(lastRestricted))
+                        item.get(Keys.DISPLAY_NAME).orElse(Text.of(item.getTranslation().get())).toBuilder()
+                                .onHover(TextActions.showItem(item))
                                 .build(),
                         Text.of(pl.getName()),
                         Text.of(location)
                 ));
-            } else if (restrictedCount > 1) {
+            } else if (resultCount.get(ResultType.RESTRICTED) > 1) {
                 pl.sendMessage(plugin.getMessageProvider().getTextMessageWithFormat(
                         "command.repair.restricted.multiple",
-                        Integer.toString(restrictedCount), pl.getName(), location
+                        resultCount.get(ResultType.RESTRICTED).toString(), pl.getName(), location
                 ));
             }
 
-            return CommandResult.successCount(successCount);
+            return CommandResult.successCount(resultCount.get(ResultType.SUCCESS));
         }
     }
 
-    private Optional<ItemStack> repairStack(ItemStack stack) {
+    private RepairResult repairStack(ItemStack stack) {
         if (whitelist && !restrictions.contains(stack.getType()) || restrictions.contains(stack.getType())) {
-            restrictedCount += 1;
-            lastRestricted = stack.createSnapshot();
-            return Optional.empty();
+            return new RepairResult(stack, ResultType.RESTRICTED);
         } else if (stack.get(DurabilityData.class).isPresent()) {
             DurabilityData durabilityData = stack.get(DurabilityData.class).get();
             DataTransactionResult transactionResult = stack.offer(Keys.ITEM_DURABILITY, durabilityData.durability().getMaxValue());
             if (transactionResult.isSuccessful()) {
-                successCount += 1;
-                lastSuccess = stack.createSnapshot();
-                return Optional.of(stack);
+                return new RepairResult(stack, ResultType.SUCCESS);
             } else {
-                errorCount += 1;
-                lastError = stack.createSnapshot();
-                return Optional.empty();
+                return new RepairResult(stack, ResultType.ERROR);
             }
         } else {
-            noDurabilityCount += 1;
-            lastNoDurability = stack.createSnapshot();
-            return Optional.empty();
+            return new RepairResult(stack, ResultType.NO_DURABILITY);
+        }
+    }
+
+    private enum ResultType {
+        SUCCESS, ERROR, RESTRICTED, NO_DURABILITY
+    }
+
+    private class RepairResult {
+
+        private ItemStack stack;
+        private ResultType type;
+
+        public RepairResult(ItemStack stack, ResultType type) {
+            this.stack = stack;
+            this.type = type;
+        }
+
+        public boolean isSuccessful() {
+            return type == ResultType.SUCCESS;
         }
     }
 }
