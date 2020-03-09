4ce2cb3a0d12325522d78d85c1238a70ac10eae5
Guus Lieben <guus@xendox.com>
2020-03-09 11:28:29 +0100

    Simplify unloaded world loading on /back
    Changes suggested by @dualspiral

--- src/main/java/io/github/nucleuspowered/nucleus/modules/back/listeners/OfflineLocation.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/back/listeners/OfflineLocation.java
@@ -33,17 +33,8 @@ public class OfflineLocation {
         if (this.lastWorld == null || this.lastRotation == null || this.lastPosition == null) {
             return Optional.empty();
         }
-        
-        Optional<WorldProperties> optionalWorldProperties = Sponge.getServer()
-                .getUnloadedWorlds().stream().filter(worldProps -> worldProps.getUniqueId().equals(this.lastWorld))
-                .findFirst();
 
-        optionalWorldProperties.ifPresent(worldProperties -> {
-            Optional<World> worldOptional = Sponge.getServer().getWorld(worldProperties.getUniqueId());
-            if (!worldOptional.isPresent()) Sponge.getServer().loadWorld(worldProperties);
-        });
-
-        return Sponge.getServer().getWorld(this.lastWorld)
+        return Sponge.getServer().loadWorld(this.lastWorld)
                 .map(world -> new Transform<>(world, this.lastPosition, this.lastRotation));
     }
 
