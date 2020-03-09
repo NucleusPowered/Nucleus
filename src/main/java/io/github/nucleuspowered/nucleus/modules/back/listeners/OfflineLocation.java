60f8e892e78df08a309fe1f4dadebdb26b21dc3e
Guus Lieben <guus@xendox.com>
2020-03-09 11:16:59 +0100

    Add unloaded world compatibility to /back
    Sponge no longer returns unloaded worlds on Sponge.getServer().getWorld(world::World)
    Solution : checking if the requested previous location is registered as unloaded, and load it if present.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/back/listeners/OfflineLocation.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/back/listeners/OfflineLocation.java
@@ -8,6 +8,7 @@ import com.flowpowered.math.vector.Vector3d;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.storage.WorldProperties;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -32,6 +33,15 @@ public class OfflineLocation {
         if (this.lastWorld == null || this.lastRotation == null || this.lastPosition == null) {
             return Optional.empty();
         }
+        
+        Optional<WorldProperties> optionalWorldProperties = Sponge.getServer()
+                .getUnloadedWorlds().stream().filter(worldProps -> worldProps.getUniqueId().equals(this.lastWorld))
+                .findFirst();
+
+        optionalWorldProperties.ifPresent(worldProperties -> {
+            Optional<World> worldOptional = Sponge.getServer().getWorld(worldProperties.getUniqueId());
+            if (!worldOptional.isPresent()) Sponge.getServer().loadWorld(worldProperties);
+        });
 
         return Sponge.getServer().getWorld(this.lastWorld)
                 .map(world -> new Transform<>(world, this.lastPosition, this.lastRotation));
