47d260867c57dc71294ef0882f3b5dfe5ebc7a4b
Daniel Naylor <git@drnaylor.co.uk>
2022-04-22 19:55:59 +0100

    Update buildscript to allow publishing v2 snapshots on GH packages

--- nucleus-api/build.gradle.kts
+++ nucleus-api/build.gradle.kts
@@ -103,14 +103,12 @@ publishing {
     }
 
     repositories {
-        if (!(rootProject.version as String).contains("SNAPSHOT")) {
-            maven {
-                name = "GitHubPackages"
-                url = uri(project.findProperty("gpr.uri") as String? ?: "${rootProject.properties["ghUri"]?.toString()!!}${System.getenv("REPO")}")
-                credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USER")
-                    password = project.findProperty("gpr.key") as String? ?: System.getenv("KEY")
-                }
+        maven {
+            name = "GitHubPackages"
+            url = uri(project.findProperty("gpr.uri") as String? ?: "${rootProject.properties["ghUri"]?.toString()!!}${System.getenv("REPO")}")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USER")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("KEY")
             }
         }
     }
